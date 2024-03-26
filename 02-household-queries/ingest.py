5680ca16be9a9fdcbbdd2accb633e57cbc1797ee
C Cheng <charliecheng@navapbc.com>
2024-03-26 18:15:46 -0400

    use google gemini, revise chroma addition

--- 02-household-queries/ingest.py
+++ 02-household-queries/ingest.py
@@ -3,7 +3,7 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import GPT4All
 from langchain import LLMChain
 from bs4 import BeautifulSoup
-from langchain_community.document_loaders import JSONLoader, PyPDFLoader
+from langchain_community.document_loaders import JSONLoader, PDFMinerLoader
 from langchain.docstore.document import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -16,44 +16,25 @@ from pathlib import Path
 from pprint import pprint
 import dotenv
 import requests
-# Implementation with Google Gemini embeddings and storing in Chroma
-dotenv.load_dotenv()
-# #Load the models
-# GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 
-# llm = ChatGoogleGenerativeAI(model="gemini-pro",
-#                              verbose = True,temperature = 0.1,google_api_key=GOOGLE_API_KEY)
-# embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+dotenv.load_dotenv()
+
+# Load the models
+GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
+llm = ChatGoogleGenerativeAI(model="gemini-pro",
+                             verbose = True,google_api_key=GOOGLE_API_KEY)
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+# initialize chroma db
+vectordb=Chroma(embedding_function=embeddings)
 
 # Load the PDF and create chunks
 # download from https://nava.slack.com/archives/C06ETE82UHM/p1710880671273909?thread_ts=1710880610.675809&cid=C06ETE82UHM
-# loader = PyPDFLoader("./7_cfr_part_272__up_to_date_as_of_3-15-2024_.pdf")
-# vectordb=Chroma.from_documents(pdf_pages,embeddings)
+loader = PDFMinerLoader("./7_cfr_part_272__up_to_date_as_of_3-15-2024_.pdf")
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
+pdf_pages = loader.load_and_split(text_splitter)
 
-# pdf_pages = loader.load_and_split(text_splitter)
-
-#Configure Chroma as a retriever with top_k=5
-# retriever = vectordb.as_retriever(search_kwargs={"k": 5})
-
-#Create the retrieval chain
-# template = """
-# You are a helpful AI assistant.
-# Answer based on the context provided. 
-# context: {context}
-# input: {input}
-# answer:
-# """
-# prompt = PromptTemplate.from_template(template)
-# combine_docs_chain = create_stuff_documents_chain(llm, prompt)
-# retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
-
-# Invoke the retrieval chain
-# response=retrieval_chain.invoke({"input":"what are appeals?"})
-
-# Print the answer to the question
-# print(response["answer"])
-
-
+vectordb.add_documents(pdf_pages)
 # Load the json and create chunks
 # download from https://drive.google.com/file/d/1UoWmktXS5nqgIWj2x_O5hgzwU0yVuaJc/view
 guru_file_path='./guru_cards_for_nava.json'
@@ -66,29 +47,29 @@ guru_file_path='./guru_cards_for_nava.json'
 #     response = requests.request("GET", url, headers=headers)
 #     return response.json()
 
-
 def get_text_chunks_langchain(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=500)
     texts = text_splitter.split_text(text)
     docs = [Document(page_content=t) for t in texts]
     return docs
 
-embeddings = SentenceTransformerEmbeddings(model_name="BAAI/bge-small-en-v1.5")
+json_data = JSONLoader(
+    file_path=guru_file_path,
+    jq_schema='.[].content',
+    text_content=False)
 
-# open json file method
-guru_data_file = open(guru_file_path)
-guru_data = json.load(guru_data_file)
+guru_data = json_data.load()
 
-guru_data_contents = []
-
+guru_data_contents = ""
 for content in guru_data:
-    soup = BeautifulSoup(content["content"], "html.parser")
+    soup = BeautifulSoup(content.page_content, "html.parser")
     text = soup.get_text(separator='\n', strip=True)
-    guru_data_contents.append(Document(page_content= text, metadata= { "source": content["preferredPhrase"] } ))
-    vectordb = Chroma.from_documents(get_text_chunks_langchain(text),embeddings, persist_directory="./chroma_db")
+    guru_data_contents += f" {text} "
 
-# Configure Chroma as a retriever with top_k=5
-retriever = vectordb.as_retriever(search_kwargs={"k": 5})
+chunks = get_text_chunks_langchain(guru_data_contents)
+vectordb.add_documents(chunks)
+
+retriever = vectordb.as_retriever(search_kwargs={"k": 1})
 
 # Create the retrieval chain
 template = """
@@ -99,14 +80,16 @@ input: {input}
 answer:
 """
 
-gpt4all_path= "./mistral-7b-instruct-v0.1.Q4_0.gguf"
-llm = GPT4All(model=gpt4all_path,max_tokens=1000, verbose=True,repeat_last_n=0)
+# Open source option
+# gpt4all_path= "./mistral-7b-instruct-v0.1.Q4_0.gguf"
+# llm = GPT4All(model=gpt4all_path,max_tokens=1000, verbose=True,repeat_last_n=0)
+# embeddings = SentenceTransformerEmbeddings(model_name="BAAI/bge-small-en-v1.5")
 prompt = PromptTemplate.from_template(template)
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 combine_docs_chain = create_stuff_documents_chain(llm, prompt)
 retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
 
 # Invoke the retrieval chain
-response=retrieval_chain.invoke({"input":"What is a disqualified household member?"})
+response=retrieval_chain.invoke({"input":"What is an appeal?"})
 
 print(response["answer"])
