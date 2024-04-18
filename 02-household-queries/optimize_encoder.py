c87be5c56a3a1390477509564d0faf2dca254035
C Cheng <charliecheng@navapbc.com>
2024-04-18 15:54:19 -0400

    incorporate feedback

--- 02-household-queries/optimize_encoder.py
+++ 02-household-queries/optimize_encoder.py
@@ -2,7 +2,7 @@ import os
 from bs4 import BeautifulSoup
 import dotenv
 from langchain.docstore.document import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter #, NLTKTextSplitter, SpacyTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter, NLTKTextSplitter, SpacyTextSplitter
 import json
 from langchain_community.embeddings import (
     SentenceTransformerEmbeddings,
@@ -15,8 +15,8 @@ from llm import ollama_client
 
 from retrieval import create_retriever
 from langchain_community.vectorstores import Chroma
-# import nltk
-# import spacy
+import nltk
+import spacy
 
 dotenv.load_dotenv()
 
@@ -51,17 +51,20 @@ def count_extra_cards(retrieved_cards, guru_cards):
 
 
 # split text into chunks
-def get_text_chunks_langchain(text, source, chunk_size, chunk_overlap, token_limit):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    # text_splitter= NLTKTextSplitter()
-    # text_splitter= SpacyTextSplitter()
+def get_text_chunks_langchain(text, source, chunk_size, chunk_overlap, token_limit, text_splitter_choice):
+    if text_splitter_choice == "2":
+        text_splitter= NLTKTextSplitter()
+    elif text_splitter_choice == "3":
+        text_splitter= SpacyTextSplitter()
+    else:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    
     texts = text_splitter.split_text(source + "\n\n" + text)
     # print("  Split into", len(texts))
-    token_total = 0
     for t in texts:
-        token_total+=llm.get_num_tokens(t)
-    if (token_total > token_limit):
-        print(f"{source} exceeded token size: {str(token_total)}")
+        token_total = llm.get_num_tokens(t)
+        if (token_total> token_limit):
+            print(f"{source} exceeded token size: {str(token_total)}")
     
     docs = [
         Document(page_content=t, metadata={"source": source.strip()}) for t in texts
@@ -71,7 +74,7 @@ def get_text_chunks_langchain(text, source, chunk_size, chunk_overlap, token_lim
 
 
 # Chunk the json data and load into vector db
-def add_json_html_data_to_vector_db(vectordb, file_path, content_key, index_key, chunk_size, chunk_overlap, token_limit):
+def add_json_html_data_to_vector_db(vectordb, file_path, content_key, index_key, chunk_size, chunk_overlap, token_limit, text_splitter_choice):
     data_file = open(file_path, encoding="utf-8")
     json_data = json.load(data_file)
     for content in json_data:
@@ -80,11 +83,11 @@ def add_json_html_data_to_vector_db(vectordb, file_path, content_key, index_key,
         soup = BeautifulSoup(content[content_key], "html.parser")
         text = soup.get_text(separator="\n", strip=True)
         # print("Processing document:", content[index_key])
-        chunks = get_text_chunks_langchain(text, content[index_key], chunk_size=chunk_size, chunk_overlap=chunk_overlap, token_limit=token_limit)
+        chunks = get_text_chunks_langchain(text, content[index_key], chunk_size=chunk_size, chunk_overlap=chunk_overlap, token_limit=token_limit, text_splitter_choice=text_splitter_choice)
         vectordb.add_documents(documents=chunks)
 
 
-def ingest_call(vectordb, chunk_size, chunk_overlap, token_limit):
+def ingest_call(vectordb, chunk_size, chunk_overlap, token_limit, text_splitter_choice):
     # download from https://drive.google.com/drive/folders/1DkAQ03bBVIPoO1d8gcHVnilQ-9VXfhJ8?usp=drive_link
     guru_file_path = "./guru_cards_for_nava.json"
     add_json_html_data_to_vector_db(
@@ -92,11 +95,11 @@ def ingest_call(vectordb, chunk_size, chunk_overlap, token_limit):
         file_path=guru_file_path,
         content_key="content",
         index_key="preferredPhrase",
-        chunk_size= chunk_size, chunk_overlap=chunk_overlap, token_limit=token_limit
+        chunk_size= chunk_size, chunk_overlap=chunk_overlap, token_limit=token_limit, text_splitter_choice=text_splitter_choice
     )
 
 
-def evaluate_retrieval(vectordb, recall_results):
+def evaluate_retrieval(vectordb):
     qa = load_training_json()
     results = []
     retriever = create_retriever(vectordb)
@@ -119,10 +122,15 @@ def evaluate_retrieval(vectordb, recall_results):
                 "extra_cards": count_extra_cards(retrieved_cards, guru_cards),
             }
         )
+    recall_results= print_and_set_recall_stats(results)
+    return recall_results
 
-    print("\nTable:")
+def print_and_set_recall_stats(results):
+    recall_results= {}
     recall = 0
     extra_card_count= 0
+    
+    print("\nTable:")
     for res in results:
         print(res["id"], "|", res["recall"], "|", res["extra_cards"])
         extra_card_count+=res["extra_cards"]
@@ -132,7 +140,7 @@ def evaluate_retrieval(vectordb, recall_results):
     recall_results["extra_card_count"] = extra_card_count
     return recall_results
 
-def run_embedding_func_and_eval_retrieval(embeddings, chunk_size, chunk_overlap):
+def run_embedding_func_and_eval_retrieval(embeddings, chunk_size, chunk_overlap, text_splitter_choice):
     selected_embedding = embeddings["func"]
     persistent_client= chromadb.PersistentClient(
             settings=Settings(allow_reset=True), path="./chroma_db"
@@ -144,18 +152,38 @@ def run_embedding_func_and_eval_retrieval(embeddings, chunk_size, chunk_overlap)
         persist_directory="./chroma_db",
         embedding_function=selected_embedding,
     )
-    ingest_call(vectordb=vectordb, chunk_size=chunk_size, chunk_overlap=chunk_overlap, token_limit= embeddings["token_limit"])
-    recall_results = {}
-    evaluate_retrieval(vectordb, recall_results)
+    ingest_call(vectordb=vectordb, chunk_size=chunk_size, chunk_overlap=chunk_overlap, token_limit= embeddings["token_limit"], text_splitter_choice=text_splitter_choice)
+    recall_results = evaluate_retrieval(vectordb)
     persistent_client.reset()
     return recall_results
 
-# nltk.download('punkt')
-# spacy.cli.download("en_core_web_sm")
+print("""
+      Choose a text splitter
+      1. RecursiveCharacterTextSplitter(Default)
+      2. NLTK
+      3. Spacy
+      """)
+
+text_splitter_choice = input()
+chunk_size=750
+chunk_overlap=300
+if text_splitter_choice == "2":
+    nltk.download('punkt')
+elif text_splitter_choice == "3":
+    spacy.cli.download("en_core_web_sm")
+else:
+    print("Define chunk size: 750(default) ")
+    chunk_size= int(input())
+    print("Define chunk overlap: 300(default) ")
+    chunk_overlap= int(input())
+
+chunk_size = 750 if chunk_size is None else chunk_size
+chunk_overlap = 300 if chunk_overlap is None else chunk_overlap
+    
 overall_results = []
 for embedding in EMBEDDINGS:
     print("Embedding: " + embedding)
-    results = run_embedding_func_and_eval_retrieval(EMBEDDINGS[embedding], chunk_size=750, chunk_overlap=300)
+    results = run_embedding_func_and_eval_retrieval(EMBEDDINGS[embedding], chunk_size=chunk_size, chunk_overlap=chunk_overlap, text_splitter_choice=text_splitter_choice)
     results["embedding_name"]= embedding
     overall_results.append(results)
 
