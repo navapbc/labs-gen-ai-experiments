75784243604b2124281a35c474933a8c1f908888
C Cheng <charliecheng@navapbc.com>
2024-04-19 10:27:32 -0400

    reformat file

--- 02-household-queries/optimize_encoder.py
+++ 02-household-queries/optimize_encoder.py
@@ -2,7 +2,11 @@ import os
 from bs4 import BeautifulSoup
 import dotenv
 from langchain.docstore.document import Document
-from langchain_text_splitters import RecursiveCharacterTextSplitter, NLTKTextSplitter, SpacyTextSplitter
+from langchain_text_splitters import (
+    RecursiveCharacterTextSplitter,
+    NLTKTextSplitter,
+    SpacyTextSplitter,
+)
 import json
 from langchain_community.embeddings import (
     SentenceTransformerEmbeddings,
@@ -22,16 +26,31 @@ dotenv.load_dotenv()
 
 _llm_model_name = os.environ.get("LLM_MODEL_NAME", "mistral")
 
-llm =  ollama_client(_llm_model_name, settings={"temperature":0.1})
+llm = ollama_client(_llm_model_name, settings={"temperature": 0.1})
 
 EMBEDDINGS = {
-     "st_all-MiniLM-L6-v2" : {"func": SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"), "token_limit":256},
-     "hf_all-MiniLM-L6-v2" : {"func": HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"), "token_limit":256},
+    "st_all-MiniLM-L6-v2": {
+        "func": SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"),
+        "token_limit": 256,
+    },
+    "hf_all-MiniLM-L6-v2": {
+        "func": HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"),
+        "token_limit": 256,
+    },
     #  "google_models/embedding-001": {"func": GoogleGenerativeAIEmbeddings(model="models/embedding-001"), "token_limit":2048},
     #  "google_models/text-embedding-004": {"func": GoogleGenerativeAIEmbeddings(model="models/text-embedding-004"), "token_limit":768},
-     "BAAI/bge-small-en-v1.5" : {"func": SentenceTransformerEmbeddings(model_name="BAAI/bge-small-en-v1.5"), "token_limit":512},
-     "mixedbread-ai/mxbai-embed-large-v1" : {"func": SentenceTransformerEmbeddings(model_name="mixedbread-ai/mxbai-embed-large-v1"), "token_limit":1024},
+    "BAAI/bge-small-en-v1.5": {
+        "func": SentenceTransformerEmbeddings(model_name="BAAI/bge-small-en-v1.5"),
+        "token_limit": 512,
+    },
+    "mixedbread-ai/mxbai-embed-large-v1": {
+        "func": SentenceTransformerEmbeddings(
+            model_name="mixedbread-ai/mxbai-embed-large-v1"
+        ),
+        "token_limit": 1024,
+    },
 }
+
 
 def load_training_json():
     with open("question_answer_citations.json", encoding="utf-8") as data_file:
@@ -51,30 +70,43 @@ def count_extra_cards(retrieved_cards, guru_cards):
 
 
 # split text into chunks
-def get_text_chunks_langchain(text, source, chunk_size, chunk_overlap, token_limit, text_splitter_choice):
+def get_text_chunks_langchain(
+    text, source, chunk_size, chunk_overlap, token_limit, text_splitter_choice
+):
     if text_splitter_choice == "2":
-        text_splitter= NLTKTextSplitter()
+        text_splitter = NLTKTextSplitter()
     elif text_splitter_choice == "3":
-        text_splitter= SpacyTextSplitter()
+        text_splitter = SpacyTextSplitter()
     else:
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size, chunk_overlap=chunk_overlap
+        )
+
     texts = text_splitter.split_text(source + "\n\n" + text)
     # print("  Split into", len(texts))
     for t in texts:
         token_count = llm.get_num_tokens(t)
-        if (token_count > token_limit):
+        if token_count > token_limit:
             print(f"Exceeded token limit of {token_limit}: {token_count}; {t}")
-    
+
     docs = [
         Document(page_content=t, metadata={"source": source.strip()}) for t in texts
     ]
-    
+
     return docs
 
 
 # Chunk the json data and load into vector db
-def add_json_html_data_to_vector_db(vectordb, file_path, content_key, index_key, chunk_size, chunk_overlap, token_limit, text_splitter_choice):
+def add_json_html_data_to_vector_db(
+    vectordb,
+    file_path,
+    content_key,
+    index_key,
+    chunk_size,
+    chunk_overlap,
+    token_limit,
+    text_splitter_choice,
+):
     data_file = open(file_path, encoding="utf-8")
     json_data = json.load(data_file)
     for content in json_data:
@@ -83,7 +115,14 @@ def add_json_html_data_to_vector_db(vectordb, file_path, content_key, index_key,
         soup = BeautifulSoup(content[content_key], "html.parser")
         text = soup.get_text(separator="\n", strip=True)
         # print("Processing document:", content[index_key])
-        chunks = get_text_chunks_langchain(text, content[index_key], chunk_size=chunk_size, chunk_overlap=chunk_overlap, token_limit=token_limit, text_splitter_choice=text_splitter_choice)
+        chunks = get_text_chunks_langchain(
+            text,
+            content[index_key],
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            token_limit=token_limit,
+            text_splitter_choice=text_splitter_choice,
+        )
         vectordb.add_documents(documents=chunks)
 
 
@@ -95,7 +134,10 @@ def ingest_call(vectordb, chunk_size, chunk_overlap, token_limit, text_splitter_
         file_path=guru_file_path,
         content_key="content",
         index_key="preferredPhrase",
-        chunk_size= chunk_size, chunk_overlap=chunk_overlap, token_limit=token_limit, text_splitter_choice=text_splitter_choice
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        token_limit=token_limit,
+        text_splitter_choice=text_splitter_choice,
     )
 
 
@@ -122,29 +164,33 @@ def evaluate_retrieval(vectordb):
                 "extra_cards": count_extra_cards(retrieved_cards, guru_cards),
             }
         )
-    recall_results= print_and_set_recall_stats(results)
+    recall_results = print_and_set_recall_stats(results)
     return recall_results
 
+
 def print_and_set_recall_stats(results):
-    recall_results= {}
+    recall_results = {}
     recall = 0
-    extra_card_count= 0
-    
+    extra_card_count = 0
+
     print("\nTable:")
     for res in results:
         print(res["id"], "|", res["recall"], "|", res["extra_cards"])
-        extra_card_count+=res["extra_cards"]
+        extra_card_count += res["extra_cards"]
         recall += res["recall"]
     recall_results["result"] = results
-    recall_results["recall_percentage"] = recall/len(results)
+    recall_results["recall_percentage"] = recall / len(results)
     recall_results["extra_card_count"] = extra_card_count
     return recall_results
 
-def run_embedding_func_and_eval_retrieval(embeddings, chunk_size, chunk_overlap, text_splitter_choice):
+
+def run_embedding_func_and_eval_retrieval(
+    embeddings, chunk_size, chunk_overlap, text_splitter_choice
+):
     selected_embedding = embeddings["func"]
-    persistent_client= chromadb.PersistentClient(
-            settings=Settings(allow_reset=True), path="./chroma_db"
-        )
+    persistent_client = chromadb.PersistentClient(
+        settings=Settings(allow_reset=True), path="./chroma_db"
+    )
 
     vectordb = Chroma(
         client=persistent_client,
@@ -152,10 +198,17 @@ def run_embedding_func_and_eval_retrieval(embeddings, chunk_size, chunk_overlap,
         persist_directory="./chroma_db",
         embedding_function=selected_embedding,
     )
-    ingest_call(vectordb=vectordb, chunk_size=chunk_size, chunk_overlap=chunk_overlap, token_limit= embeddings["token_limit"], text_splitter_choice=text_splitter_choice)
+    ingest_call(
+        vectordb=vectordb,
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        token_limit=embeddings["token_limit"],
+        text_splitter_choice=text_splitter_choice,
+    )
     recall_results = evaluate_retrieval(vectordb)
     persistent_client.reset()
     return recall_results
+
 
 print("""
       Choose a text splitter
@@ -165,26 +218,31 @@ print("""
       """)
 
 text_splitter_choice = input()
-chunk_size=750
-chunk_overlap=300
+chunk_size = 750
+chunk_overlap = 300
 if text_splitter_choice == "2":
-    nltk.download('punkt')
+    nltk.download("punkt")
 elif text_splitter_choice == "3":
     spacy.cli.download("en_core_web_sm")
 else:
     print("Define chunk size: 750(default) ")
-    chunk_size= int(input())
+    chunk_size = int(input())
     print("Define chunk overlap: 300(default) ")
-    chunk_overlap= int(input())
+    chunk_overlap = int(input())
 
 chunk_size = 750 if chunk_size is None else chunk_size
 chunk_overlap = 300 if chunk_overlap is None else chunk_overlap
-    
+
 overall_results = []
 for embedding in EMBEDDINGS:
     print("Embedding: " + embedding)
-    results = run_embedding_func_and_eval_retrieval(EMBEDDINGS[embedding], chunk_size=chunk_size, chunk_overlap=chunk_overlap, text_splitter_choice=text_splitter_choice)
-    results["embedding_name"]= embedding
+    results = run_embedding_func_and_eval_retrieval(
+        EMBEDDINGS[embedding],
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        text_splitter_choice=text_splitter_choice,
+    )
+    results["embedding_name"] = embedding
     overall_results.append(results)
 
 max_recall = max(overall_results, key=lambda x: x["recall_percentage"])
