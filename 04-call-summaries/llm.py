3703d46e1d5ca982ffb3288c1045fb5e69b86534
C Cheng <charliecheng@navapbc.com>
2024-05-06 13:18:24 -0400

    add llm selection tool and base transcript

---
+++ 04-call-summaries/llm.py
@@ -0,0 +1,55 @@
+import dotenv
+
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.llms.ollama import Ollama
+import google.generativeai as genai
+from openai import OpenAI
+import os
+
+dotenv.load_dotenv()
+
+def ollama_client(model_name=None, prompt="", callbacks=None, settings=None, print_to_stdout=False):
+    if not callbacks:
+        callbacks = []
+    if print_to_stdout:
+        callbacks.append(StreamingStdOutCallbackHandler())
+
+    if not settings:
+        settings = {
+            # "temperature": 0.1,
+            # "system": "",
+            # "template": "",
+            # See https://github.com/langchain-ai/langchain/blob/master/libs/community/langchain_community/llms/ollama.py
+            "stop": None
+        }
+
+    print("LLM settings:", model_name, settings)
+    # To connect via another URL: Ollama(base_url='http://localhost:11434', ...)
+    return Ollama(model=model_name, callbacks=callbacks, **settings).invoke(prompt)
+
+
+def google_gemini_client(model_name="gemini-pro", prompt ="", callbacks=None, settings={}, print_to_stdout=False):
+    # Get a Google API key by following the steps after clicking on Get an API key button
+    # at https://ai.google.dev/tutorials/setup
+    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+
+    print("LLM settings:", model_name, settings)
+
+    genai.configure(api_key=GOOGLE_API_KEY)
+    genai.GenerationConfig(**settings)
+    model = genai.GenerativeModel(model_name)
+    return model.generate_content(prompt)
+    
+
+def gpt3_5(prompt, model="gpt-3.5-turbo"):
+    OPEN_AI_API_KEY = os.environ.get("OPEN_AI_API_KEY")
+    openai_client = OpenAI(api_key=OPEN_AI_API_KEY)  # Uses OPENAI_API_KEY
+    return (
+        openai_client.chat.completions.create(model=model, messages=[{"role": "user", "content": prompt}])
+        .choices[0]
+        .message.content
+    )
+
+
+def gpt_4_turbo(prompt):
+    return gpt3_5(prompt, model="gpt-4-turbo")
