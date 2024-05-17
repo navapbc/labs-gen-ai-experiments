693052020649cb5fdec2b8dfd5ad0237215d1acd
C Cheng <charliecheng@navapbc.com>
2024-05-17 14:08:46 -0400

    create function for stuffing code

--- 04-call-summaries/run.py
+++ 04-call-summaries/run.py
@@ -39,104 +39,104 @@ Fill out the fields with the text given {text}. If the following template alread
 Please fill out the data with the following template: {template}
 """
 
-print("""
-      Select an llm
-      1. openhermes (default)
-      2. dolphin
-      3. gemini
-      4. gpt 4
-      5. gpt 4o
-      6. claude 3
-      7. all
-      """)
 
 transcript = get_transcript()
-llm = input() or "1"
 prompt_template = PromptTemplate.from_template(PROMPT)
 formatted_prompt = prompt_template.format(transcript=transcript)
 
-if llm == "2":
-    ollama = ollama_client(model_name="dolphin-mistral")
-    response = ollama_client(client=ollama, prompt=formatted_prompt)
-    print("""----------
-        Dolphin
-        """)
-elif llm == "3":
-    gemini = google_gemini_client()
-    response = google_gemini_client(client=gemini, prompt=formatted_prompt)
-    print("""----------
-        Gemini Flash 1.5
-        """)
-elif llm == "4":
-    print("""----------
-      GPT 4
-      """)
-    gpt = gpt_client()
-    response = gpt_client(client=gpt, model_choice="gpt4", prompt=formatted_prompt)
-elif llm == "5":
-    print("""----------
-      GPT 4o
-      """)
-    gpt = gpt_client()
-    response = gpt_client(client=gpt, model_choice="gpt4o", prompt=formatted_prompt)
-elif llm == "6":
-    print("""----------
-        Claude 3
-        """)
-    claude = claude_client()
-    response = claude_client(client=claude, prompt=formatted_prompt)
-elif llm == "7":
-    # print("""
-    #     Openhermes
-    #     """)
-    # ollama_openhermes = ollama_client(model_name="openhermes")
-    # ollama_openhermes_response = ollama_client(
-    #     client=ollama_openhermes, prompt=formatted_prompt
-    # )
-    # print(ollama_openhermes_response)
 
-    # print("""----------
-    #     Dolphin
-    #     """)
-    # ollama_dolphin = ollama_client(model_name="dolphin-mistral")
-    # dolphin_response = ollama_client(client=ollama_dolphin, prompt=formatted_prompt)
-    # print(dolphin_response)
-
-    # print("""----------
-    #     Gemini Flash 1.5
-    #     """)
-    # gemini = google_gemini_client()
-    # gemini_response = google_gemini_client(client=gemini, prompt=formatted_prompt)
-    # print(gemini_response)
-
-    # gpt = gpt_client()
-    # print("""----------
-    #   GPT 4
-    #   """)
-    # gpt_4_response = gpt_client(
-    #     client=gpt, model_choice="gpt4", prompt=formatted_prompt
-    # )
-    # print(gpt_4_response)
-
-    # print("""----------
-    #   GPT 4o
-    #   """)
-    # gpt_4o_response = gpt_client(
-    #     client=gpt, model_choice="gpt-4o", prompt=formatted_prompt
-    # )
-    # print(gpt_4o_response)
-
-    print("""----------
-        Claude 3
-        """)
-    claude = claude_client()
-    claude_response = claude_client(client=claude, prompt=formatted_prompt)
-    print(claude_response)
-else:
-    ollama = ollama_client(model_name="openhermes")
-    response = ollama_client(client=ollama, prompt=formatted_prompt)
+def stuffing_summary(prompt=None):
     print("""
-        Openhermes
+        Select an llm
+        1. openhermes (default)
+        2. dolphin
+        3. gemini
+        4. gpt 4
+        5. gpt 4o
+        6. claude 3
+        7. all
         """)
-if response:
-    print(response)
+
+    llm = input() or "1"
+
+    if llm == "2":
+        ollama = ollama_client(model_name="dolphin-mistral")
+        response = ollama_client(client=ollama, prompt=prompt)
+        print("""----------
+            Dolphin
+            """)
+    elif llm == "3":
+        gemini = google_gemini_client()
+        response = google_gemini_client(client=gemini, prompt=prompt)
+        print("""----------
+            Gemini Flash 1.5
+            """)
+    elif llm == "4":
+        print("""----------
+        GPT 4
+        """)
+        gpt = gpt_client()
+        response = gpt_client(client=gpt, model_choice="gpt4", prompt=prompt)
+    elif llm == "5":
+        print("""----------
+        GPT 4o
+        """)
+        gpt = gpt_client()
+        response = gpt_client(client=gpt, model_choice="gpt4o", prompt=prompt)
+    elif llm == "6":
+        print("""----------
+            Claude 3
+            """)
+        claude = claude_client()
+        response = claude_client(client=claude, prompt=prompt)
+    elif llm == "7":
+        print("""
+            Openhermes
+            """)
+        ollama_openhermes = ollama_client(model_name="openhermes")
+        ollama_openhermes_response = ollama_client(
+            client=ollama_openhermes, prompt=prompt
+        )
+        print(ollama_openhermes_response)
+
+        print("""----------
+            Dolphin
+            """)
+        ollama_dolphin = ollama_client(model_name="dolphin-mistral")
+        dolphin_response = ollama_client(client=ollama_dolphin, prompt=prompt)
+        print(dolphin_response)
+
+        print("""----------
+            Gemini Flash 1.5
+            """)
+        gemini = google_gemini_client()
+        gemini_response = google_gemini_client(client=gemini, prompt=prompt)
+        print(gemini_response)
+
+        gpt = gpt_client()
+        print("""----------
+        GPT 4
+        """)
+        gpt_4_response = gpt_client(client=gpt, model_choice="gpt4", prompt=prompt)
+        print(gpt_4_response)
+
+        print("""----------
+        GPT 4o
+        """)
+        gpt_4o_response = gpt_client(client=gpt, model_choice="gpt-4o", prompt=prompt)
+        print(gpt_4o_response)
+
+        print("""----------
+            Claude 3
+            """)
+        claude = claude_client()
+        claude_response = claude_client(client=claude, prompt=prompt)
+        print(claude_response)
+    else:
+        ollama = ollama_client(model_name="openhermes")
+        response = ollama_client(client=ollama, prompt=prompt)
+        print("""
+            Openhermes
+            """)
+    if response:
+        print(response)
