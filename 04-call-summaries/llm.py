26a079a39ce4cbede253c2b3cba5a0df8afca039
C Cheng <charliecheng@navapbc.com>
2024-05-21 10:52:48 -0400

    refactor llm choices into class

--- 04-call-summaries/llm.py
+++ 04-call-summaries/llm.py
@@ -9,87 +9,98 @@ from openai import OpenAI
 dotenv.load_dotenv()
 
 
-def get_transcript(file_path="./transcript.txt"):
-    file = open(file_path, encoding="utf-8")
-    content = file.read()
-    return content
+class LLM:
+    def __init__(
+        self,
+        client_name=None,
+        model_name=None,
+        max_tokens=1024,
+        settings=None,
+    ):
+        self.client_name = client_name
+        """Name of llm selection"""
+        self.model_name = model_name
+        """User friendly model name"""
+        self.model_version = model_name
+        """Exact model name being passed into the initializer"""
+        self.max_tokens = max_tokens
+        self.client = None
+        self.settings = settings
 
+    def get_client(self):
+        """Retrieves the llm client"""
+        if self.client_name == "ollama":
+            if not settings:
+                settings = {
+                    # "temperature": 0.1,
+                    # "system": "",
+                    # "template": "",
+                    # See https://github.com/langchain-ai/langchain/blob/master/libs/community/langchain_community/llms/ollama.py
+                    "stop": None
+                }
+            if self.model_name is None:
+                self.model_name = "openhermes"
+            # To connect via another URL: Ollama(base_url='http://localhost:11434', ...)
+            self.client = Ollama(model=self.model_version, **self.settings)
 
-def ollama_client(
-    model_name=None, prompt=None, callbacks=None, settings=None, client=None
-):
-    if not settings:
-        settings = {
-            # "temperature": 0.1,
-            # "system": "",
-            # "template": "",
-            # See https://github.com/langchain-ai/langchain/blob/master/libs/community/langchain_community/llms/ollama.py
-            "stop": None
-        }
+        elif self.client_name == "gemini":
+            # Get a Google API key by following the steps after clicking on Get an API key button
+            # at https://ai.google.dev/tutorials/setup
+            GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+            print("LLM settings:", self.model_name, settings)
 
-    print("LLM settings:", model_name, settings)
-    # To connect via another URL: Ollama(base_url='http://localhost:11434', ...)
+            if self.model_version is None:
+                self.model_version = "gemini-1.5-flash-latest"
+            elif self.model_name == "gemini-pro":
+                self.model_version = "gemini-1.5-pro-latest"
 
-    if client:
-        return client.invoke(prompt)
-    return Ollama(model=model_name, callbacks=callbacks, **settings)
+            genai.configure(api_key=GOOGLE_API_KEY)
+            if settings:
+                genai.GenerationConfig(**self.settings)
+            self.client = genai.GenerativeModel(self.model_name)
 
+        elif self.client_name == "gpt":
+            if self.model_name is None:
+                self.model_version = "gpt-4o"
+            elif self.model_name == "gpt3":
+                self.model_version = "gpt-3.5-turbo"
+            elif self.model_name == "gpt4":
+                self.model_version = "gpt-4-turbo"
 
-def google_gemini_client(
-    model_name="gemini-1.5-flash-latest", prompt=None, settings=None, client=None
-):
-    # Get a Google API key by following the steps after clicking on Get an API key button
-    # at https://ai.google.dev/tutorials/setup
-    GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+            # Get API key from https://platform.openai.com/api-keys
+            OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+            self.client = OpenAI(api_key=OPENAI_API_KEY)  # Uses OPENAI_API_KEY
 
-    print("LLM settings:", model_name, settings)
-
-    genai.configure(api_key=GOOGLE_API_KEY)
-    if settings:
-        genai.GenerationConfig(**settings)
-    if client:
-        return client.generate_content(prompt).text
-    return genai.GenerativeModel(model_name)
-
-
-def gpt_client(prompt=None, model_choice="gpt-4o", client=None):
-    if client:
-        if model_choice == "gpt3":
-            model = "gpt-3.5-turbo"
-        elif model_choice == "gpt4":
-            model = "gpt-4-turbo"
-        else:
-            model = model_choice
-        return (
-            client.chat.completions.create(
-                model=model, messages=[{"role": "user", "content": prompt}]
+        elif self.client_name == "claude":
+            self.model_version = "claude-3-opus-20240229"
+            ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
+            self.client = anthropic.Anthropic(
+                api_key=ANTHROPIC_API_KEY,
             )
-            .choices[0]
-            .message.content
-        )
-    else:
-        # Get API key from https://platform.openai.com/api-keys
-        OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-        return OpenAI(api_key=OPENAI_API_KEY)  # Uses OPENAI_API_KEY
 
+    def generate_text(self, prompt=None):
+        """Generates response given prompt"""
+        if self.client_name == "ollama":
+            return self.client.invoke(prompt)
+        elif self.client_name == "gemini":
+            return self.client.generate(prompt).text
+        elif self.client_name == "gpt":
+            return (
+                self.client.chat.completions.create(
+                    model=self.model_version,
+                    messages=[{"role": "user", "content": prompt}],
+                )
+                .choices[0]
+                .message.content
+            )
+        elif self.client_name == "claude":
+            generated_response = self.client.messages.create(
+                model=self.model_version,
+                max_tokens=self.max_tokens,
+                messages=[{"role": "user", "content": prompt}],
+            ).content
+            text_response = "\n".join(
+                [text_block.text for text_block in generated_response]
+            )
 
-def claude_client(
-    prompt=None, model="claude-3-opus-20240229", max_tokens=1024, client=None
-):
-    if client:
-        generated_response = client.messages.create(
-            model=model,
-            max_tokens=max_tokens,
-            messages=[{"role": "user", "content": prompt}],
-        ).content
-        text_response = "\n".join(
-            [text_block.text for text_block in generated_response]
-        )
-
-        return text_response
-    else:
-        # Get API key from https://console.anthropic.com/settings/keys
-        ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
-        return anthropic.Anthropic(
-            api_key=ANTHROPIC_API_KEY,
-        )
+            return text_response
