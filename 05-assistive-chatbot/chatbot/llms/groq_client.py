6f722f77bc5e546def5ddf2e0c671bc9d259e4d5
Yoom Lam <yoom@navapbc.com>
2024-06-08 18:01:17 -0500

    fix GroqClient's initial args

--- 05-assistive-chatbot/chatbot/llms/groq_client.py
+++ 05-assistive-chatbot/chatbot/llms/groq_client.py
@@ -20,12 +20,15 @@ def init_client(model_name, settings):
 
 
 class GroqClient:
+    INIT_ARGS = ["timeout", "max_retries", "default_headers", "default_query", "base_url", "http_client"]
+
     def __init__(self, model_name, settings):
         self.model_name = model_name
         self.settings = settings
-        logger.info("Creating LLM client '%s' with %s", model_name, self.settings)
-        # TODO: remove temperature from settings
-        self.client = Groq(**self.settings)
+
+        init_settings = {k: settings[k] for k in settings if k in self.INIT_ARGS}
+        logger.info("Creating LLM client '%s' with %s", model_name, init_settings)
+        self.client = Groq(**init_settings)
 
     def generate_reponse(self, message):
         chat_completion = self.client.chat.completions.create(
